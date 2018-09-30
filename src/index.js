7695e69c48da7ad8665569701153e0cd3cd9aac8
Aliaksei Navamlinets <alex.novomlinets@gmail.com>
2018-09-30 14:46:53 +0300

    feat: implement feature according to the task#11

--- src/index.js
+++ src/index.js
@@ -1,26 +1,83 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.value = initialValue;
   }
 
   add(number) {
-    // your implementation
+    this.value += " + " + number;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.value += " - " + number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.value += " * " + number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.value += " / " + number;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.value += " ^ " + number;
+    return this;
+  }
+
+  calculate() {
+    let num = 0;
+    this.value = this.value.split(' ');
+    while (this.value.length > 1) {
+      for (let op = this.value.length - 1; op >= 0; op--) {
+        if (this.value[op] === "^") {
+          num = Math.pow(this.value[op - 1], this.value[op + 1]);
+          this.value.splice(op, 2);
+          this.value[op - 1] = num;
+          op++;
+        }
+      }
+      for (let op = 0; op < this.value.length; op++) {
+        if (this.value[op] === "*") {
+          num = this.value[op - 1] * this.value[op + 1];
+          this.value.splice(op, 2);
+          this.value[op - 1] = num;
+          op--;
+        }
+      }
+      for (let op = 0; op < this.value.length; op++) {
+        if (this.value[op] === "/") {
+          num = Math.round(this.value[op - 1] / this.value[op + 1]);
+          this.value.splice(op, 2);
+          this.value[op - 1] = num;
+          op--;
+        }
+      }
+      for (let op = 0; op < this.value.length; op++) {
+        if (this.value[op] === "-") {
+          num = parseInt(this.value[op - 1]) - parseInt(this.value[op + 1]);
+          this.value.splice(op, 2);
+          this.value[op - 1] = num;
+          op--;
+        }
+      }
+      for (let op = 0; op < this.value.length; op++) {
+        if (this.value[op] === "+") {
+          num = parseInt(this.value[op - 1]) + parseInt(this.value[op + 1]);
+          this.value.splice(op, 2);
+          this.value[op - 1] = num;
+          op--;
+        }
+      }
+    }
+    return this.value[0];
+  }
+
+  valueOf() {
+    return this.calculate();
   }
 }
 
